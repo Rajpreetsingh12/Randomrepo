f362e9fb8f272275752fe1f435c161b6ae2662d7
unknown <rajpreetsinghjahagirdar59@gmail.com>
2024-04-06 18:21:54 +0530

    this is my first commit to python.py

---
+++ python.py
@@ -0,0 +1,21 @@
+operations = {
+
+    'add': lambda x, y: x + y,
+
+    'subtract': lambda x, y: x - y,
+
+    'multiply': lambda x, y: x * y,
+
+    'divide': lambda x, y: x / y
+
+}
+
+ 
+
+print(operations['add'](5, 3))       
+
+print(operations['subtract'](10, 4))  
+
+print(operations['multiply'](2, 6))    
+
+print(operations['divide'](10, 2))
